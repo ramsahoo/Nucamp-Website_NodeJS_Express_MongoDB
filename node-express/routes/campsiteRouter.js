c04f4cca72f2e91a2f5547fe525e745274d7cab4
Rameen Sahoo <rameensahoo@gmail.com>
2022-01-29 09:26:39 -0800

    Node MongoDB Example 2

--- node-express/routes/campsiteRouter.js
+++ node-express/routes/campsiteRouter.js
@@ -21,4 +21,24 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send you campsite ${req.params.campsiteId}`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on ${req.params.campsiteId}`);
+})
+.put((req, res) => {
+    res.end(`Will update the campsite ${req.params.campsiteId}: ${req.body.name} with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end('Deleting campsite');
+});
+
 module.exports = campsiteRouter;
